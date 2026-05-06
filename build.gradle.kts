b05e9e66e0fb3f6742e0cba7bcb41360672d75d3
Sevval Yilmaz <sevvalyilmazcontact@gmail.com>
2026-05-06 11:54:51 +0200

    chore: add environment variable configuration for datasource

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("me.paulschwarz:spring-dotenv:4.0.0")
 
 	runtimeOnly("com.h2database:h2")
 
