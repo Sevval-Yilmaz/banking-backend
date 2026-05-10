75602975ae88254001a6af2139f2b0236742ffa7
Sevval Yilmaz <sevvalyilmazcontact@gmail.com>
2026-05-10 19:03:54 +0200

    refactor: clean code

--- src/main/java/com/example/learning/user_demo/mapper/UserMapper.java
+++ src/main/java/com/example/learning/user_demo/mapper/UserMapper.java
@@ -1,6 +1,5 @@
 package com.example.learning.user_demo.mapper;
 
-import com.example.learning.user_demo.dto.UserRequestDto;
 import com.example.learning.user_demo.dto.UserResponseDto;
 import com.example.learning.user_demo.entity.User;
 import org.mapstruct.Mapper;
