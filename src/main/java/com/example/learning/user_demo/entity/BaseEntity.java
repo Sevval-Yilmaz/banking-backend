de7905aa87096453e50dcd0c7662c65d25cd42e1
Sevval Yilmaz <sevvalyilmazcontact@gmail.com>
2026-05-10 18:54:10 +0200

    refactor: introduce BaseEntity for shared id handling

---
+++ src/main/java/com/example/learning/user_demo/entity/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.example.learning.user_demo.entity;
+
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+}
